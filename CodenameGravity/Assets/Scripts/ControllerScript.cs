5905e0afd70bbb31500265c1022a7c26d81be58d
Rajan Saini <rajansaini73@hotmail.com>
2015-08-21 16:41:30 -0600

    Initial Build

---
+++ CodenameGravity/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ControllerScript: MonoBehaviour {
+	
+	[HideInInspector] public bool facingRight = true;
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public Transform groundCheck;
+
+	private Dictionary<int, Vector3> gravityList = new Dictionary<int, Vector3>();
+	private int gravity = 0;
+	private bool grounded = false;
+	private Animator anim;
+	private Rigidbody2D rb2d;
+	
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		anim = GetComponent<Animator>();
+		rb2d = GetComponent<Rigidbody2D>();
+		gravityList.Add(0, new Vector3(0, -30));
+		gravityList.Add(1, new Vector3(30, 0));
+		gravityList.Add(2, new Vector3(0, 30));
+		gravityList.Add(3, new Vector3(-30, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+	}
+	
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+		
+		anim.SetFloat("Speed", Mathf.Abs(h));
+		
+		if (h * rb2d.velocity.x < maxSpeed && (gravity == 0 || gravity == 2)) {
+			if (gravity == 0)
+				rb2d.AddForce (Vector2.right * h * moveForce);
+			if (gravity == 2)
+				rb2d.AddForce (Vector2.left * h * moveForce);
+		}
+
+		if (h * rb2d.velocity.y < maxSpeed && (gravity == 1 || gravity == 3)) {
+			if (gravity == 1)
+				rb2d.AddForce (Vector2.up * h * moveForce);
+			if (gravity == 3)
+				rb2d.AddForce (Vector2.down * h * moveForce);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed && (gravity == 0 || gravity == 2))
+			rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		if (Mathf.Abs (rb2d.velocity.y) > maxSpeed && (gravity == 1 || gravity == 3))
+			rb2d.velocity = new Vector2(rb2d.velocity.x, Mathf.Sign (rb2d.velocity.y) * maxSpeed);
+		
+		if (h > 0 && !facingRight)
+			Flip ();
+		else if (h < 0 && facingRight)
+			Flip ();
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) {
+			CycleGravity();
+			SetGravity ();
+		}
+	}
+
+	void CycleGravity()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			gravity += 1;
+			if (gravity > 3)
+				gravity = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			gravity -= 1;
+			if (gravity < 0)
+				gravity = 3;
+		}
+	}
+
+	void SetGravity()
+	{
+		Vector3 temp;
+		gravityList.TryGetValue(gravity, out temp);
+		Physics2D.gravity = temp;
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
