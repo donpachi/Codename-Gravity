a8b02f2ac0bd8bf518117ea2818c9b2be8fd0aa8
donpachi <aaron.th.kim@gmail.com>
2015-08-22 15:07:43 -0600

    Added testing functionality + textures
    Added the ability move the player using a + d keys as well as added
    background physx object to prevent out of world.

--- menu_v0.1/Assets/Scripts/Controls.cs
+++ menu_v0.1/Assets/Scripts/Controls.cs
@@ -14,12 +14,12 @@ public class Controls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// If right side of screen is touched
-		if (Input.touchCount == 1 && Input.GetTouch (0).position.x >= Screen.currentResolution.width / 2) {
+		if ((Input.touchCount == 1 && Input.GetTouch (0).position.x >= Screen.currentResolution.width / 2) || Input.GetKey("d")) {
 			Vector3 right = new Vector3(1, 0, 0);
 			rb.AddForce(right * thrust, ForceMode.Impulse);
 		}
 		// If left side of screen is touched
-		if (Input.touchCount == 1 && Input.GetTouch (0).position.x < Screen.currentResolution.width / 2) {
+		if ((Input.touchCount == 1 && Input.GetTouch (0).position.x < Screen.currentResolution.width / 2) || Input.GetKey("a")) {
 			Vector3 left = new Vector3(-1, 0, 0);
 			rb.AddForce(left * thrust, ForceMode.Impulse);
 		}
