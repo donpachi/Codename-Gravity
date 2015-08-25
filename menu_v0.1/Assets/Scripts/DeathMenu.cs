feb3bf2ddc1048499b5b9e2f872db2f3e0ac89c1
Raj73 <rajansaini73@hotmail.com>
2015-08-25 00:32:47 -0600

    Finished Death Menu

--- menu_v0.1/Assets/Scripts/DeathMenu.cs
+++ menu_v0.1/Assets/Scripts/DeathMenu.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class DeathMenu : MonoBehaviour {
 
-	private GameObject Death;
 	private GameObject Controller;
+	private GameObject Death;
+
+	void Start()
+	{
+		Player character = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		Death = GameObject.Find("DeathCanvas");
+		Controller = GameObject.Find("ControlCanvas");
+		AddListener (character);
+	}
+
+	private void AddListener (Player character)
+	{
+		character.OnPlayerDeath += HandleOnPlayerDeath;
+	}
+
+	private void HandleOnPlayerDeath()
+	{
+		Time.timeScale = 0;
+		Death.GetComponent<Canvas>().enabled = true;
+		Controller.GetComponent<Canvas>().enabled = false;
+	}
 
 	public void Restart()
 	{
-		Death = GameObject.Find("DeathCanvas");
-		Controller = GameObject.Find("ControlCanvas");
 		Death.GetComponent<Canvas>().enabled = false;
 		Controller.GetComponent<Canvas>().enabled = true;
 		Time.timeScale = 1;
