0b76dafc02fc54bd210e48abf74b76c094dc2f27
Arthur Lee <ruhtraeel@gmail.com>
2015-08-24 18:46:44 -0600

    Hazards prototype pushed

---
+++ menu_v0.1/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	private bool alive = true;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void addForce(Vector3 vector) {
+
+	}
+
+	public bool getStatus() {
+		return alive;
+	}
+
+	public void setStatus(bool status) {
+		alive = status;
+	}
+
+	void OnCollisionEnter(Collision obj) {
+		if (obj.gameObject.tag == "Hazard") {
+			alive = false;
+		}
+	}
+
+
+}
