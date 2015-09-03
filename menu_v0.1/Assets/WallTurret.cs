23ba588f1216e14215faada952ba629b157e8237
Yosuke Saito <yosuke.saito@outlook.com>
2015-09-03 16:31:59 -0600

    Wall turret stuff.

---
+++ menu_v0.1/Assets/WallTurret.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTurret : MonoBehaviour {
+
+	public GameObject projectile;
+	private float timer;
+	private Vector3 spawnLocation;
+
+	//Publicly changable variables
+	public float FireRate;
+	public float InitialDelay;
+	public float ProjectileForce;
+	public Vector3 spawnOffset;
+
+	// Use this for initialization
+	void Start () {
+		timer = InitialDelay;
+		spawnLocation = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0) {
+
+		}
+	}
+}
