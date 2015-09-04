8b8fc3740471f24dd638740a38bc17d87a60bd06
Yosuke Saito <yosuke.saito@outlook.com>
2015-09-04 01:07:16 -0600

    Fixed up vaccum and windtunnel script. changed sprite to standard size

--- menu_v0.1/Assets/Scripts/Vaccum.cs
+++ menu_v0.1/Assets/Scripts/Vaccum.cs
@@ -1,52 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WindTunnel : MonoBehaviour {
-
+public class Vaccum : MonoBehaviour {
+	
 	private bool inTunnelPath;
 	private GameObject player;
-	private float windForce;
-	private Animator anim;
-
+	private float vacForce;
+	
 	//Adjustable variables
-	public float MaxWindForce;
-	public float MaxWindDistance; //size of the box collider
+	public float MaxVacForce;
+	public float MaxVacDistance; //size of the box collider
 	public Vector2 Direction; //Sets the default orientation or of the force, will rotate with game object
-
+	
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		inTunnelPath = false;
-		windForce = 1;
+		vacForce = 1;
 		Direction = gameObject.GetComponent<Transform> ().rotation * Direction;
-		anim = gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (inTunnelPath) {
-			addWindForce();
+			addVacForce();
 		}
 	}
-
-	void addWindForce(){
-
+	
+	void addVacForce(){
+		
 		float distance = Vector2.Distance (player.GetComponent<Transform> ().position, gameObject.GetComponent<Transform> ().position);
 
-		windForce = (MaxWindDistance - distance)/ MaxWindDistance * MaxWindForce;
+		vacForce = (MaxVacDistance -  Mathf.Abs(distance)) / MaxVacDistance * MaxVacForce;
 
-		if (windForce < 0) {
-			windForce = 0;
+		if (vacForce < 0) {
+			vacForce = 0;
 		}
 
-		player.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * windForce);
-	}
+		Debug.Log ("distance: " + distance + " force: " + vacForce);
 
+		player.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * vacForce);
+	}
+	
 	void OnTriggerEnter2D(Collider2D collision){
 		inTunnelPath = true;
-		anim.SetTrigger ("StartTurbine");
 	}
-
+	
 	void OnTriggerExit2D(Collider2D collision){
 		inTunnelPath = false;
 	}
