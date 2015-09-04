6602c17c4bbd05ee3cda49c1342f2f68eaf334ee
Raj73 <rajansaini73@hotmail.com>
2015-09-03 19:17:11 -0600

    Added Changes to Gravity Cannon

--- menu_v0.1/Assets/Scripts/GravityCannon.cs
+++ menu_v0.1/Assets/Scripts/GravityCannon.cs
@@ -2,29 +2,35 @@
 using System.Collections;
 
 public class GravityCannon : MonoBehaviour {
-
-	private GameObject player;
-	private Rigidbody playerBody;
+	
+	private Rigidbody2D playerBody;
 	private bool activated = false;
-	private float ENTRYDISTANCE = 1.5f;
 	private float LAUNCHFORCE = 5f;
 	
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player");
-		//playerBody = player.GetComponent<Rigidbody>
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance (player.GetComponent<Transform> ().position, this.GetComponent<Transform> ().position);
-
-		if (distance < ENTRYDISTANCE)
-			activated = true;
-
 		if (activated) {
 			//playerBody.
 		}
 
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		playerBody = collisionInfo.rigidbody;
+
+		if (collisionInfo.gameObject.tag == "Player")
+		{
+			activated = true;
+			Vector3 newPosition = new Vector3 (playerBody.position.x,
+				                               playerBody.position.y,
+				                               1);
+			playerBody.GetComponent<Transform>().position = (newPosition);
+			playerBody.Sleep();
+		}
 	}
 }
