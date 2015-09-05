3bcebd867e5ace467089cd5a5b55126b2ca5edf7
Raj73 <rajansaini73@hotmail.com>
2015-09-05 00:52:24 -0600

    Progress made on Cannon entity:   animation, positioning, and controls finished
    Still need to add: launch force and zero gravity flight

--- menu_v0.1/Assets/Scripts/GravityCannon.cs
+++ menu_v0.1/Assets/Scripts/GravityCannon.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GravityCannon : MonoBehaviour {
 
-	Animator anim;
-	int cannonActivate = Animator.StringToHash("CannonActivate");
+	private Animator anim;
 	private GameObject player;
 	private Rigidbody2D playerBody;
+	public GameObject cannonTip;
+	public GameObject[] buttons;
+
 	private bool activated = false;
 	private float LAUNCHFORCE = 5f;
 	
@@ -18,28 +21,45 @@ public class GravityCannon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (activated) {
-
-		}
 
 	}
 
-	void OnCollisionEnter2D(Collision2D collisionInfo)
-	{
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
 		playerBody = collisionInfo.rigidbody;
-
-		if (collisionInfo.gameObject.tag == "Player")
-		{
-			activated = true;
-			playerBody.Sleep();
-			Vector3 newPosition = new Vector3 (playerBody.position.x,
-				                               playerBody.position.y,
+		if (collisionInfo.gameObject.tag == "Player") {
+			Vector3 newPosition = new Vector3 (this.transform.position.x + 2.5f,
+			                                   this.transform.position.y + 1.0f,
 				                               1);
 			playerBody.GetComponent<Transform>().position = (newPosition);
-			playerBody.Sleep();
 			player.GetComponent<Controls>().resetMovementFlags();
+			playerBody.gravityScale = 0f;
+			playerBody.Sleep();
+
+			activated = true;
 			anim.SetBool("activated", true);
 
+			for (int i = 0; i < buttons.Length - 1; i++) {
+				buttons[i].SetActive(false);
+			}
+			buttons[buttons.Length - 1].SetActive(true);
 		}
+	}
+
+	public void FireDown() {
+		Vector3 newPosition = new Vector3 (cannonTip.transform.position.x,
+		                                   cannonTip.transform.position.y,
+		                                   -2);
+		playerBody.GetComponent<Transform>().position = (newPosition);
+		playerBody.WakeUp();
+		playerBody.gravityScale = 1f;
+
+		activated = false;
+		anim.SetBool("activated", false);
+		
+		for (int i = 0; i < buttons.Length - 1; i++) {
+			buttons[i].SetActive(true);
+		}
+		buttons[buttons.Length - 1].SetActive(false);
+
 	}
 }
