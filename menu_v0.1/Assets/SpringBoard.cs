d67f40abdd494db2ed60f4e13da7eac1c33d9db3
Yosuke Saito <yosuke.saito@outlook.com>
2015-09-10 20:09:34 -0600

    Added a Spring board

---
+++ menu_v0.1/Assets/SpringBoard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringBoard : MonoBehaviour {
+
+    private GameObject player;
+
+    public float SpringForce;
+    public Vector2 Direction;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Direction = gameObject.GetComponent<Transform>().rotation * Direction;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        player.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * SpringForce);
+    }
+}
