7c52bd60409ac9075cb006fbefe7eda9452ab6f1
Raj73 <rajansaini73@hotmail.com>
2015-09-10 22:55:56 -0600

    Added Breakable Wall Prefab

---
+++ menu_v0.1/Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableWall : MonoBehaviour {
+
+	public Animator anima;
+	public float breakThreshold;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionInfo) {
+		if (collisionInfo.gameObject.tag == "Boulder") {
+			this.GetComponent<Collider2D>().enabled = false;
+			if (collisionInfo.relativeVelocity.magnitude > breakThreshold) {
+				anima.SetTrigger("destroy");
+			}
+		}
+	}
+
+	public void eraseWall()
+	{
+		this.gameObject.SetActive(false);
+	}
+}
