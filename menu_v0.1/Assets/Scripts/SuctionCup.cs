df0aa5e8a6788946999b647f8252016e2c4ffb76
Rajan Saini <rajansaini73@hotmail.com>
2015-09-14 16:21:42 -0600

    Finished controls for SuctionCup boots
    Signed-off-by: Rajan Saini <rajansaini73@hotmail.com>

--- menu_v0.1/Assets/Scripts/SuctionCup.cs
+++ menu_v0.1/Assets/Scripts/SuctionCup.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SuctionCup : MonoBehaviour {
 
 	public GameObject player;
+    public int suctionTimer = 10;
 
 	private ConstantForce2D playerConsForce;
 	private Vector2 suctionVector;
@@ -19,12 +20,20 @@ public class SuctionCup : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collisionInfo) {
-		print ("hello");
 		if (collisionInfo.gameObject.tag == "Player") {
 			suctionVector = Physics2D.gravity;
 			collisionInfo.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
 			playerConsForce = player.GetComponent<ConstantForce2D>();
 			playerConsForce.relativeForce = suctionVector * 2;
+            this.gameObject.SetActive(false);
+            player.GetComponent<Controls>().SuctionCupOn();
+
+            Invoke("SuctionStatusEnd", suctionTimer);
+
+            playerConsForce.relativeForce = new Vector2(0, 0);
+            collisionInfo.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+
+            player.GetComponent<Controls>().SuctionStatusEnd();
 		}
 	}
 }
