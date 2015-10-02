5ef0a0aebf9fb871ab58d0aa3e93a06407dcde82
donpachii <aaron.th.kim@gmail.com>
2015-10-01 23:44:41 -0600

    Finished  main menu transition functionality

--- menu_v0.1/Assets/Scripts/MenuTransition.cs
+++ menu_v0.1/Assets/Scripts/MenuTransition.cs
@@ -1,30 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using System.Collections;
 
-public class MenuTransition : MonoBehaviour {
+public class MenuTransition : MonoBehaviour
+{
 
-    public bool released;
+    private bool released;
+    private RectTransform contentViewer;
+    private RectTransform closestScreenRect;
+    private GameObject[] levelSelectScreens;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        contentViewer = this.GetComponent<RectTransform>();
         released = false;
-	}
+        levelSelectScreens = GameObject.FindGameObjectsWithTag("Level Select");
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (released)
         {
-            this.transform.position = Vector3.Lerp(this.transform.position, this.GetComponentsInChildren<Transform>()[1].position, 0);
+            contentViewer.offsetMax = new Vector2 (Mathf.Lerp(contentViewer.offsetMax.magnitude,
+                                                               closestScreenRect.offsetMax.magnitude, 
+                                                               15.0f * Time.deltaTime), 0f) * -1;
+            contentViewer.offsetMin = new Vector2 (Mathf.Lerp(contentViewer.offsetMin.magnitude,
+                                                               closestScreenRect.offsetMax.magnitude, 
+                                                               15.0f * Time.deltaTime), 0f) * -1;
         }
-        if (Vector3.Distance(this.transform.position, this.GetComponentsInChildren<Transform>()[1].position) < 0.001f)
+
+        if (released && Vector2.Distance(contentViewer.offsetMax, closestScreenRect.offsetMax * -1) < 0.05f)
         {
             released = false;
+            this.GetComponent<RectTransform>().offsetMax = closestScreenRect.offsetMax * -1; //center position: MAY BE REDUNDANT
+            this.GetComponent<RectTransform>().offsetMin = closestScreenRect.offsetMax * -1;
         }
 	}
 
-    public void OnEndDrag(PointerEventData eventData)
+    public void EndDrag()
     {
+        float distance = float.MaxValue;
+        float tempDistance;
+        for (int i = 0; i < levelSelectScreens.Length; i++)
+        {
+            tempDistance = Vector2.Distance(contentViewer.offsetMax, levelSelectScreens[i].GetComponent<RectTransform>().offsetMax * -1);
+
+            if (distance > tempDistance)
+            {
+                distance = tempDistance;
+                closestScreenRect = levelSelectScreens[i].GetComponent<RectTransform>();
+            }
+        }
         released = true;
+    }
+
+    public void BeginDrag()
+    {
+        released = false;
     }
 }
