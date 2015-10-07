2eb75ed9230a4f2ef706122b6bba9317f0d80607
Arthur Lee <ruhtraeel@gmail.com>
2015-10-06 18:11:35 -0600

    Initial raycasting fixed

--- menu_v0.1/Assets/Scripts/Water.cs
+++ menu_v0.1/Assets/Scripts/Water.cs
@@ -10,8 +10,9 @@ public class Water : MonoBehaviour {
 	float DISAPPEARTHRESHOLD = 0.05f;
 	bool resizing = false;
 	float originalLengthOfWaterBody = 0f;
-	DeviceOrientation previousOrientation;
+	OrientationListener.Orientation previousOrientation;
 	int frameCounter = 0;
+    float RAYCASTCOLLISIONDISTANCE = 0.486f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,41 +20,7 @@ public class Water : MonoBehaviour {
 		controls = GameObject.FindGameObjectWithTag("Player").GetComponent<Controls>();
 		AddListener (controls);
 		originalLengthOfWaterBody = this.GetComponent<SpriteRenderer>().bounds.size.x;
-		previousOrientation = Input.deviceOrientation;
-	}
-
-	// Vector array [left, right, up, down]
-	Vector2 [] returnVectors() {
-		Vector2 [] vectorArray = new Vector2[4];
-		if (Input.deviceOrientation == DeviceOrientation.Portrait) {
-			vectorArray[0] = Vector2.left;
-			vectorArray[1] = Vector2.right;
-			vectorArray[2] = Vector2.up;
-			vectorArray[3] = Vector2.down;
-			return vectorArray;
-		}
-		else if (Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) {
-			vectorArray[0] = Vector2.right;
-			vectorArray[1] = Vector2.left;
-			vectorArray[2] = Vector2.down;
-			vectorArray[3] = Vector2.up;
-			return vectorArray;
-		}
-		else if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft) {
-			vectorArray[0] = Vector2.up;
-			vectorArray[1] = Vector2.down;
-			vectorArray[2] = Vector2.right;
-			vectorArray[3] = Vector2.left;
-			return vectorArray;
-		}
-		else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight) {
-			vectorArray[0] = Vector2.down;
-			vectorArray[1] = Vector2.up;
-			vectorArray[2] = Vector2.left;
-			vectorArray[3] = Vector2.right;
-			return vectorArray;
-		}
-		return null;
+        previousOrientation = OrientationListener.instanceOf.currentOrientation();
 	}
 
 	private void AddListener (Controls controls)
@@ -74,34 +41,40 @@ public class Water : MonoBehaviour {
 		Vector2 leftRayOrigin;
 		Vector2 rightRayOrigin;
 
-		if (Input.deviceOrientation == DeviceOrientation.Portrait) { // if (controls.getOrientationInfo() == "portrait") {
-			leftRayOrigin = new Vector2 (this.transform.position.x - Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.x), this.transform.position.y);
-			leftsidehit = Physics2D.Raycast (leftRayOrigin, Vector2.left);
-			rightRayOrigin = new Vector2 (this.transform.position.x + Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.x), this.transform.position.y);
-			rightsidehit = Physics2D.Raycast (rightRayOrigin, Vector2.right);
-		} else if (Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) {
-			leftRayOrigin = new Vector2 (this.transform.position.x + Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.x), this.transform.position.y);
-			leftsidehit = Physics2D.Raycast (leftRayOrigin, Vector2.right);
-			rightRayOrigin = new Vector2 (this.transform.position.x - Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.x), this.transform.position.y);
-			rightsidehit = Physics2D.Raycast (rightRayOrigin, Vector2.left);
-		} else if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft) {//if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight) { //if (controls.getOrientationInfo() == "landscape") {
-			leftRayOrigin = new Vector2 (this.transform.position.y - Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.y), this.transform.position.x);
-			leftsidehit = Physics2D.Raycast (leftRayOrigin, Vector2.up);
-			rightRayOrigin = new Vector2 (this.transform.position.y + Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.y), this.transform.position.x);
-			rightsidehit = Physics2D.Raycast (rightRayOrigin, Vector2.down);
-		} else {// (Input.deviceOrientation == DeviceOrientation.LandscapeRight) {
-			leftRayOrigin = new Vector2 (this.transform.position.y + Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.y), this.transform.position.x);
-			leftsidehit = Physics2D.Raycast (leftRayOrigin, Vector2.down);
-			rightRayOrigin = new Vector2 (this.transform.position.y - Math.Abs (this.GetComponent<SpriteRenderer> ().bounds.min.y), this.transform.position.x);
-			rightsidehit = Physics2D.Raycast (rightRayOrigin, Vector2.up);
-		}
-		//Physics2D.Raycast (this.transform, Vector2.down, 1, 1 << LayerMask.NameToLayer)("Walls");
-		Vector2 scale = this.transform.localScale;
+
+        if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.PORTRAIT)
+        {
+            leftRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.min.x, this.transform.position.y);          
+            rightRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.max.x, this.transform.position.y);
+        }
+        else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.INVERTED_PORTRAIT)
+        {
+            leftRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.max.x, this.transform.position.y);
+            rightRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.min.x, this.transform.position.y);
+        }
+        else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_LEFT)
+        {
+            leftRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.max.y);
+            rightRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.min.y);
+            
+        }
+        else
+        {
+            leftRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.min.y);
+            rightRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.max.y);
+            
+        }
+        leftsidehit = Physics2D.Raycast(leftRayOrigin, OrientationListener.instanceOf.getRelativeLeftVector(), RAYCASTCOLLISIONDISTANCE, 1 << LayerMask.NameToLayer("Walls"));
+        rightsidehit = Physics2D.Raycast(rightRayOrigin, OrientationListener.instanceOf.getRelativeRightVector(), RAYCASTCOLLISIONDISTANCE, 1 << LayerMask.NameToLayer("Walls"));
+
+        
+        //Physics2D.Raycast (this.transform, Vector2.down, 1, 1 << LayerMask.NameToLayer("Walls"));
+        Vector2 scale = this.transform.localScale;
 		Vector2 position = this.transform.position;
 		BoxCollider2D waterCollider = (BoxCollider2D)this.GetComponent<BoxCollider2D> ();
 
 
-		/*
+        /*
 		// If there is a wall only on the left side of the water body
 		if (leftsidehit.distance < 5 && rightsidehit.distance > 5) {
 			if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) {
@@ -144,13 +117,12 @@ public class Water : MonoBehaviour {
 			}
 		} 
 		*/
-		// If there are no walls on either side of the water body
-		if (leftsidehit.distance > 0.000000000000000000000000000001f && rightsidehit.distance > 0.000000000000000000000000000000001f ){
-		
-			if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) { //if (controls.getOrientationInfo() == "portrait") {
+        // If there are no walls on either side of the water body
+        if (!leftsidehit && !rightsidehit){
+			if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.INVERTED_PORTRAIT || OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.PORTRAIT) {
 				scale.y = scale.y * WATERRUNOFFRATE;
 				scale.x = scale.x * 1 / WATERRUNOFFRATE;
-			} else if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight) { //else if (controls.getOrientationInfo () == "landscape") {
+			} else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_LEFT || OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_RIGHT) {
 				scale.x = scale.x * WATERRUNOFFRATE;
 				scale.y = scale.y * 1 / WATERRUNOFFRATE;
 			}
@@ -161,65 +133,34 @@ public class Water : MonoBehaviour {
 			//	Destroy (this);
 			//}
 		}
-		// If there are walls on both sides of the water body
-		//else {
-			//resizing = false;
-			/*
-			// Spawn another water source
-			GameObject newParticleGenerator=(GameObject)Instantiate(Resources.Load("LiquidPhysics/ParticleGenerator"));
-			if (leftsidehit.distance > 1) {
-				newParticleGenerator.transform.position = leftRayOrigin;
-			}
-			if (rightsidehit.distance > 1) {
-				newParticleGenerator.transform.position = rightRayOrigin;
-			}*/
-			
-		//}
-	}
+        // If there are walls on both sides of the water body
+        else
+        {
+            resizing = false;
+            /*
+        	// Spawn another water source
+        	GameObject newParticleGenerator=(GameObject)Instantiate(Resources.Load("LiquidPhysics/ParticleGenerator"));
+        	if (leftsidehit.distance > 1) {
+        		newParticleGenerator.transform.position = leftRayOrigin;
+        	}
+        	if (rightsidehit.distance > 1) {
+        		newParticleGenerator.transform.position = rightRayOrigin;
+        	}*/
 
-	public void combineWater(Transform childWaterBody) {
-		Vector2 scale = this.transform.localScale;
-		if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown) { // if (controls.getOrientationInfo () == "portrait") {
-			while (this.GetComponent<SpriteRenderer>().bounds.size.x < originalLengthOfWaterBody + childWaterBody.GetComponent<SpriteRenderer>().bounds.size.x) {
-				scale.x = this.GetComponent<Transform>().localScale.x + 0.1f;
-
-			}
-		} 
-		else if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight) { // else if (controls.getOrientationInfo () == "landscape") {
-			while (this.GetComponent<SpriteRenderer>().bounds.size.y < originalLengthOfWaterBody + childWaterBody.GetComponent<SpriteRenderer>().bounds.size.y) {
-				scale.y = this.GetComponent<Transform>().localScale.y + 0.1f;
-			}
-		}
-		this.transform.localScale = scale;
-	}
-	
-
-	public void OnCollision2D(Collider2D colliderEvent) {
-		if (colliderEvent.tag == tag) {
-		}
-			//resizing = false;
-		/*
-		else {
-			Transform childWaterBody = colliderEvent.GetComponentInChildren<Transform>();
-			combineWater(childWaterBody);
-			Destroy (colliderEvent.GetComponentInChildren<GameObject>());
-
-		}*/
-	}
+        }
+    }
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 
-		// To be deleted once controls is fixed
-		if (previousOrientation != Input.deviceOrientation && frameCounter % 6 == 0) {
+		if (previousOrientation != OrientationListener.instanceOf.currentOrientation()) {
 			resizing = true;
 		}
 		if (resizing == true) {
 			resizeWater ();
 		}
-		if (Input.deviceOrientation != DeviceOrientation.Unknown && Input.deviceOrientation != DeviceOrientation.FaceDown && Input.deviceOrientation != DeviceOrientation.FaceUp)
-			previousOrientation = Input.deviceOrientation;
+	    previousOrientation = OrientationListener.instanceOf.currentOrientation();
 		frameCounter++;
 	}
 }
