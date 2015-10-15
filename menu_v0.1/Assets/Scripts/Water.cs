07f0e9cae5cbe884bd7c900ddbf3c4e5adbd7d35
Arthur Lee <ruhtraeel@gmail.com>
2015-10-15 15:47:10 -0600

    Fixed gravity cooldown & added water resizing

--- menu_v0.1/Assets/Scripts/Water.cs
+++ menu_v0.1/Assets/Scripts/Water.cs
@@ -5,20 +5,18 @@ using System;
 public class Water : MonoBehaviour {
 	
     //Controls controls;
-    float WATERRUNOFFRATE = 0.9f;
+    float WATERRUNOFFRATE = 0.05f;
     //float waterScale = 1f;
     //float DISAPPEARTHRESHOLD = 0.05f;
     bool resizing = false;
-    //float originalLengthOfWaterBody = 0f;
     OrientationListener.Orientation previousOrientation;
     //int frameCounter = 0;
     float RAYCASTCOLLISIONDISTANCE = 5f;//0.490f;
-    float absoluteYPosition;
+    float heightAnchor;
 
     //// Use this for initialization
     void Start()
     {
-
         //controls = GameObject.FindGameObjectWithTag("Player").GetComponent<Controls>();
         //originalLengthOfWaterBody = this.GetComponent<SpriteRenderer>().bounds.size.x;
         previousOrientation = OrientationListener.instanceOf.currentOrientation();
@@ -39,26 +37,26 @@ public class Water : MonoBehaviour {
         {
             leftRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.min.x, this.transform.position.y);
             rightRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.max.x, this.transform.position.y);
-            absoluteYPosition = this.GetComponent<SpriteRenderer>().bounds.min.y;
+            heightAnchor = this.GetComponent<SpriteRenderer>().bounds.min.y;
         }
         else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.INVERTED_PORTRAIT)
         {
             leftRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.max.x, this.transform.position.y);
             rightRayOrigin = new Vector2(this.GetComponent<SpriteRenderer>().bounds.min.x, this.transform.position.y);
-            absoluteYPosition = this.GetComponent<SpriteRenderer>().bounds.max.y;
+            heightAnchor = this.GetComponent<SpriteRenderer>().bounds.max.y;
         }
         else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_LEFT)
         {
             leftRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.max.y);
             rightRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.min.y);
-            absoluteYPosition = this.GetComponent<SpriteRenderer>().bounds.min.x;
+            heightAnchor = this.GetComponent<SpriteRenderer>().bounds.min.x;
 
         }
         else
         {
             leftRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.min.y);
             rightRayOrigin = new Vector2(this.transform.position.x, this.GetComponent<SpriteRenderer>().bounds.max.y);
-            absoluteYPosition = this.GetComponent<SpriteRenderer>().bounds.max.y;
+            heightAnchor = this.GetComponent<SpriteRenderer>().bounds.max.y;
 
         }
         leftsidehit = Physics2D.Raycast(leftRayOrigin, OrientationListener.instanceOf.getRelativeLeftVector(), RAYCASTCOLLISIONDISTANCE, 1 << LayerMask.NameToLayer("Walls"));
@@ -117,7 +115,7 @@ public class Water : MonoBehaviour {
         else if (leftsidehit.collider == null && rightsidehit)
         {
             Debug.Log("No wall on left side");
-            createSpawners(rightRayOrigin);
+            createSpawners(leftRayOrigin);
     
         }
 
@@ -154,6 +152,8 @@ public class Water : MonoBehaviour {
             {
                 GameObject spawnerAbove = (GameObject)Instantiate(Resources.Load("ParticleSource"));
                 GameObject spawnerBelow = (GameObject)Instantiate(Resources.Load("ParticleSource"));
+                spawnerAbove.transform.parent = this.transform;
+                spawnerBelow.transform.parent = this.transform;
                 Vector2 originOffsetAbove = new Vector2(origin.x - distanceFromOrigin, origin.y);
                 Vector2 originOffsetBelow = new Vector2(origin.x + distanceFromOrigin, origin.y);
                 spawnerAbove.transform.position = originOffsetAbove;
@@ -164,8 +164,10 @@ public class Water : MonoBehaviour {
         {
             for (float distanceFromOrigin = 0; distanceFromOrigin < (this.GetComponent<SpriteRenderer>().bounds.size.y / 2); distanceFromOrigin += 2)
             {
-                GameObject spawnerAbove = (GameObject)Instantiate(Resources.Load("ParticleSource"));
+                GameObject spawnerAbove = (GameObject)Instantiate(Resources.Load("ParticleSource"));               
                 GameObject spawnerBelow = (GameObject)Instantiate(Resources.Load("ParticleSource"));
+                spawnerAbove.transform.parent = this.transform;
+                spawnerBelow.transform.parent = this.transform;
                 Vector2 originOffsetAbove = new Vector2(origin.x, origin.y - distanceFromOrigin);
                 Vector2 originOffsetBelow = new Vector2(origin.x, origin.y + distanceFromOrigin);
                 spawnerAbove.transform.position = originOffsetAbove;
@@ -180,37 +182,54 @@ public class Water : MonoBehaviour {
         Vector2 scale = this.transform.localScale;
         if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.INVERTED_PORTRAIT || OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.PORTRAIT)
         {
-            scale.y = scale.y - 0.3f;//* WATERRUNOFFRATE;      
-            //this.transform.position = new Vector2(this.transform.position.x, absoluteYPosition);
+            scale.y = scale.y - WATERRUNOFFRATE;      
+            if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.INVERTED_PORTRAIT)
+                this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + WATERRUNOFFRATE);
+            else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.PORTRAIT)
+                this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y - WATERRUNOFFRATE);
         }
         else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_LEFT || OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_RIGHT)
         {
-            scale.x = scale.x * WATERRUNOFFRATE;
-            //this.transform.position = new Vector2(this.transform.position.x, absoluteYPosition);
+            scale.x = scale.x - WATERRUNOFFRATE;
+            //foreach (Transform child in this.transform)
+            //{
+            //    if (this.GetComponent<SpriteRenderer>().bounds.min.x == child.GetComponent<SpriteRenderer>().bounds.min.x)//the x value of a water spawner)
+            //    {
+            //        Destroy(child);
+            //    }
+            //}
+            
+            if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_RIGHT)
+            {
+                this.transform.position = new Vector2(this.transform.position.x + WATERRUNOFFRATE, this.transform.position.y);
+                
+            }
+            else if (OrientationListener.instanceOf.currentOrientation() == OrientationListener.Orientation.LANDSCAPE_LEFT)
+                this.transform.position = new Vector2(this.transform.position.x - WATERRUNOFFRATE, this.transform.position.y);
         }
         this.transform.localScale = scale;
         
+        
+
+
         if (scale.x <= 0 || scale.y <= 0)
         {
             Destroy(gameObject);
+            resizing = false;
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(OrientationListener.instanceOf.currentOrientation());
-
         if (previousOrientation != OrientationListener.instanceOf.currentOrientation())
-        {
-            
-            
+        {       
             checkToSpawn();
             resizing = true;
         }
         if (resizing == true)
         {
-            //resizeWater();
+            resizeWater();
         }
         previousOrientation = OrientationListener.instanceOf.currentOrientation();
 	}
--- menu_v0.1/Assets/Scripts/WorldGravity.cs
+++ menu_v0.1/Assets/Scripts/WorldGravity.cs
@@ -29,12 +29,13 @@ public class WorldGravity : MonoBehaviour {
         elapsedTime += Time.deltaTime;
         if (elapsedTime > GRAVITYCOOLDOWN) {
             gravityOnCooldown = false;
-            if (!gravityOnCooldown && (previousGravityDirection != OrientationListener.instanceOf.currentOrientation())){
+        }
+        if (!gravityOnCooldown && (previousGravityDirection != OrientationListener.instanceOf.currentOrientation())){
                 updateGravity();
                 gravityOnCooldown = true;
                 elapsedTime = 0;
             }
-        }
+        
 	}
 
     void initialize()
