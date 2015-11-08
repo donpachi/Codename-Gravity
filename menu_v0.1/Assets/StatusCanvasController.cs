10b9f8c30ad13c79ca72dd555425f0202b5475bf
Yosuke Saito <yosuke.saito@outlook.com>
2015-11-08 14:38:41 -0700

    moving gravity cooldown to status canvas

--- menu_v0.1/Assets/StatusCanvasController.cs
+++ menu_v0.1/Assets/StatusCanvasController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StatusCanvasController : MonoBehaviour {
 
@@ -12,13 +13,37 @@ public class StatusCanvasController : MonoBehaviour {
     private bool gravityCooldownFlag;
     private bool suctionCounterFlag;
 
+    private GameObject suctionText;
+    private GameObject gcText;
+
     private float SCCounter = 0;
     private float GCCounter = 0;
+
+    //Event functions
+    public delegate void SCTimerEvent();
+    public static event SCTimerEvent SCEnd;
+
+    public delegate void GCEvent();
+    public static event GCEvent GCEnd;
+
+    void triggerSCEnd()
+    {
+        if (SCEnd != null)
+            SCEnd();
+    }
+
+    void triggerGCEnd()
+    {
+        if (GCEnd != null)
+            GCEnd();
+    }
 
 	// Use this for initialization
 	void Start () {
         suctionCounterFlag = false;
         gravityCooldownFlag = false;
+        suctionText = transform.FindChild("SuctionText").gameObject;
+        gcText = transform.FindChild("GravityCooldown").gameObject;
 	}
 	
 	// Update is called once per frame
@@ -31,18 +56,32 @@ public class StatusCanvasController : MonoBehaviour {
 
     void updateSCCounter()
     {
+        SCCounter -= Time.deltaTime;
+        
         if (SCCounter <= 0)
         {
             suctionCounterFlag = false;
+            suctionText.GetComponent<Text>().text = "";
         }
+        else
+            suctionText.GetComponent<Text>().text = SCCounter.ToString();
     }
 
     void updateGCCounter()
     {
+        GCCounter -= Time.deltaTime;
+
+        if (GCCounter <= 0)
+        {
+            gravityCooldownFlag = false;
+            gcText.GetComponent<Text>().text = "";
+        }
+        else
+            gcText.GetComponent<Text>().text = GCCounter.ToString();
 
     }
 
-    //update all the objects in canvas to the correct orientation
+    //WorldGravity Listener: updates all the objects in canvas to the correct orientation
     void updateCanvasObjectOrientation(OrientationListener.Orientation orientation, float timer)
     {
         foreach (Transform child in transform)
@@ -72,12 +111,14 @@ public class StatusCanvasController : MonoBehaviour {
     {
         suctionCounterFlag = true;
         SCCounter = timer;
+        suctionText.GetComponent<Text>().text = timer.ToString();
     }
 
     void setGCTimer(float timer)
     {
         gravityCooldownFlag = true;
         GCCounter = timer;
+        gcText.GetComponent<Text>().text = timer.ToString();
     }
 
     //Listeners for canvas
