b84c9f0b793dfbf27addd2d0a02200a20a2f7e30
Arthur Lee <s_s-money@hotmail.com>
2015-11-19 18:05:35 -0700

    Pressure plate reformatted to contain canBeUntriggered, door script created

--- CGR/Assets/Scripts/PressurePlate.cs
+++ CGR/Assets/Scripts/PressurePlate.cs
@@ -1,67 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PressurePlate : MonoBehaviour {
 
-    bool resizingDoor = false;
-    float RESIZERATE = 0.08f;
+    public delegate void PlateTriggeredHandler();
+    public bool canBeUntriggered = false;
+    public static event PlateTriggeredHandler Enter;
+    public static event PlateTriggeredHandler Exit;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (resizingDoor == true)
-            resizeDoor();
+
 	}
 
-    void OnCollisionEnter2D(Collision2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.GetComponent<Rigidbody2D>().mass >= 1) 
+        OnEnter();
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (canBeUntriggered == true)
         {
-            
-            // Door opens
-            foreach (Transform transform in this.GetComponentsInChildren<Transform>()) 
-            {
-                if (transform.gameObject.name == "ShrinkingDoor")
-                {
-                    
-                    resizingDoor = true;
-                }
-            }
-           
+            OnExit();
         }
     }
 
-    void resizeDoor()
+    void OnEnter()
     {
-        // Door opens
-        foreach (Transform transform in this.GetComponentsInChildren<Transform>())
+        if (Enter != null)
         {
-            if (transform.gameObject.name == "ShrinkingDoor")
-            {
-                Vector2 scale = transform.gameObject.transform.localScale;
-                if (scale.x <= 0 || scale.y <= 0)
-                {
-                    Destroy(transform.gameObject);
-                }
-                if (Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.max.x) - Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.min.x) > Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.max.y) - Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.min.y))
-                {
-                    scale = new Vector2(scale.x, scale.y - RESIZERATE);
-                    transform.gameObject.transform.position = new Vector2(transform.gameObject.transform.position.x, transform.gameObject.transform.position.y - RESIZERATE);
-                }
-                else
-                {
-                    scale = new Vector2(scale.x - RESIZERATE, scale.y);
-                    transform.gameObject.transform.position = new Vector2(transform.gameObject.transform.position.x - RESIZERATE, transform.gameObject.transform.position.y);
-                }
-                transform.gameObject.transform.localScale = scale;
-            }
+            Enter();
         }
-        if (this.GetComponentsInChildren<Transform>().Length == 0)
-            resizingDoor = false;
     }
 
+    void OnExit()
+    {
+        if (Exit != null)
+        {
+            Exit();
+        }
+    }
 }
--- CGR/Assets/Scripts/ShrinkingDoor.cs
+++ CGR/Assets/Scripts/ShrinkingDoor.cs
@@ -1,15 +1,98 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
 
 public class ShrinkingDoor : MonoBehaviour {
 
+    GameObject parent;
+    public float resizespeed = 0.08f;
+    bool shrinking = false;
+    bool growing = false;
+    Vector2 originalScale;
+    string originalOrientation;
+
 	// Use this for initialization
 	void Start () {
-	
+        originalScale = transform.localScale;
+        parent = this.GetComponentInParent<Transform>().gameObject;
+        PressurePlate.Enter += triggerDoorShrink;
+        PressurePlate.Exit += triggerDoorGrow;
+        if (Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.max.x) - Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.min.x) > Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.max.y) - Mathf.Abs(gameObject.GetComponent<SpriteRenderer>().bounds.min.y))
+        {
+            originalOrientation = "widthwise";
+        }
+        else
+        {
+            originalOrientation = "heightwise";
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (shrinking == true)
+        {
+            shrinkDoor();
+            growing = false;
+        }
+        if (growing == true)
+        {
+            growDoor();
+            shrinking = false;
+        }
 	}
+
+    void triggerDoorShrink()
+    {
+        shrinking = true;
+    }
+
+    void shrinkDoor()
+    {
+        Vector2 scale = transform.localScale;
+        if (scale.x <= 0.1 || scale.y <= 0.1)
+        {
+            shrinking = false;
+            return;
+        }
+        if (originalOrientation == "widthwise")
+        {
+            scale = new Vector2(scale.x - resizespeed, scale.y);
+            transform.position = new Vector2(transform.gameObject.transform.position.x - resizespeed, transform.gameObject.transform.position.y);
+        }
+        else
+        {
+            scale = new Vector2(scale.x, scale.y - resizespeed);
+            transform.position = new Vector2(transform.gameObject.transform.position.x, transform.gameObject.transform.position.y - resizespeed);
+        }
+        Debug.Log("Shrinking: " + transform.localScale);
+        transform.localScale = scale;
+    }
+
+    void triggerDoorGrow()
+    {
+        growing = true;
+    }
+
+    void growDoor()
+    {
+        Vector2 scale = transform.localScale;
+        if (scale.x >= originalScale.x && scale.y >= originalScale.y)
+        {
+            growing = false;
+            return;
+        }
+        if (originalOrientation == "widthwise")
+        {
+            scale = new Vector2(scale.x + resizespeed, scale.y);
+            transform.position = new Vector2(transform.gameObject.transform.position.x + resizespeed, transform.gameObject.transform.position.y);
+        }
+        else
+        {
+            scale = new Vector2(scale.x, scale.y + resizespeed);
+            transform.position = new Vector2(transform.gameObject.transform.position.x, transform.gameObject.transform.position.y + resizespeed);
+        }
+        Debug.Log("Growing:" + transform.localScale);
+        transform.localScale = scale;
+    }
 }
