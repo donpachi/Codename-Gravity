f42c41df059eb10a768edab90991036a3bca7270
Rajan Saini <rajansaini73@hotmail.com>
2015-11-19 17:40:39 -0700

    Modified background to include Parallax scrolling

---
+++ CGR/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D player;
+    private float horizontal = 0;
+    private float vertical = 0;
+
+    void Start () {
+        speed = speed / 10000;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal += (player.velocity.x * speed);
+        vertical += (player.velocity.y * speed);
+        this.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(horizontal, vertical);
+    }
+}
