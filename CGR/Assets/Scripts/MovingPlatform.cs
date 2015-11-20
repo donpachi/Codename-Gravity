ebfb76b8e6b77cc8c6acd3a39b4f804ae5feaea9
Arthur Lee <s_s-money@hotmail.com>
2015-11-20 01:01:04 -0700

    Fixed moving platforms so direction flags don't fire if not in use + logic fix for changing directions

--- CGR/Assets/Scripts/MovingPlatform.cs
+++ CGR/Assets/Scripts/MovingPlatform.cs
@@ -5,70 +5,78 @@ public class MovingPlatform : MonoBehaviour {
 
     public bool MoveRight;
     public bool MoveDown;
-    public int XDistance;
-    public int YDistance;
+    public int XDistance = -1;
+    public int YDistance = -1;
     private int XDistRemain;
     private int YDistRemain;
     public float speed;
     GameObject parent;
+    public bool isActive;
 
 	// Use this for initialization
 	void Start () {
         XDistRemain = XDistance;
         YDistRemain = YDistance;
-        parent = this.GetComponentInParent<Transform>().gameObject;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (parent.name == "PressurePlate")
+        if (isActive == true)
         {
+            Vector2 vector = this.transform.position;
+            if (MoveRight == false && XDistance != -1)
+            {
+                if (XDistRemain == 0)
+                {
+                    MoveRight = true;
+                    XDistRemain = XDistance;
+                }
+                vector = new Vector2(vector.x - speed, vector.y);
+                XDistRemain--;
+                
+            }
+            else if (MoveRight == true && XDistance != -1)
+            {
+                if (XDistRemain == 0)
+                {
+                    MoveRight = false;
+                    XDistRemain = XDistance;
+                }
+                vector = new Vector2(vector.x + speed, vector.y);
+                XDistRemain--;
+                
+            }
 
-        }
-        Vector2 vector = this.transform.position;
-        if (MoveRight == false)
-        {
-            vector = new Vector2(vector.x - speed, vector.y);
-            XDistRemain--;
-            if (XDistRemain <= 0)
+            if (MoveDown == true && YDistance != -1)
             {
-                MoveRight = true;
-                XDistRemain = XDistance;
+                if (YDistRemain == 0)
+                {
+                    MoveDown = false;
+                    YDistRemain = YDistance;
+                }
+                vector = new Vector2(vector.x, vector.y - speed);
+                YDistRemain--;
+                
             }
-        }
-        else 
-        {
-            vector = new Vector2(vector.x + speed, vector.y);
-            XDistRemain--;
-            if (XDistRemain <= 0)
+            else if (MoveDown == false && YDistance != -1)
             {
-                MoveRight = false;
-                XDistRemain = XDistance;
+                if (YDistRemain == 0)
+                {
+                    MoveDown = true;
+                    YDistRemain = YDistance;
+                }
+                vector = new Vector2(vector.x, vector.y + speed);
+                YDistRemain--;
+                
             }
-        }
 
-        if (MoveDown == true)
-        {
-            vector = new Vector2(vector.x, vector.y - speed);
-            YDistRemain--;
-            if (YDistRemain <= 0)
-            {
-                MoveDown = false;
-                YDistRemain = YDistance;
-            }
+
+            this.transform.position = vector;
         }
-        else
-        {
-            vector = new Vector2(vector.x, vector.y + speed);
-            YDistRemain--;
-            if (YDistRemain <= 0)
-            {
-                MoveDown = true;
-                YDistRemain = YDistance;
-            }
-        }
-        
-        
-        this.transform.position = vector;
 	}
+
+    void plateDepressed()
+    {
+        isActive = true;
+    }
 }
