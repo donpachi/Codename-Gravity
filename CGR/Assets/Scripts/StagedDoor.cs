3db1422eef812568750cdb9cd5020312a392a639
Arthur Lee <s_s-money@hotmail.com>
2016-01-27 17:04:19 -0700

    Updated stageddoor script

--- CGR/Assets/Scripts/StagedDoor.cs
+++ CGR/Assets/Scripts/StagedDoor.cs
@@ -19,56 +19,100 @@ public class StagedDoor : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        GameObject parent = this.transform.parent.gameObject;
         if (collapsing == true && growing == false)
+            collapseDoor();
+        else if (collapsing == false && growing == true)
+            growDoor();
+    }
+
+    void collapseDoor()
+    {
+        GameObject parent = this.transform.parent.gameObject;
+        if (transform.childCount == 0 && this.GetComponent<SpriteRenderer>().enabled == true)
         {
-            if (transform.childCount == 0 && this.GetComponent<SpriteRenderer>().enabled == true)
+            this.GetComponent<StagedDoor>().isActive = true;
+            parent.GetComponent<StagedDoor>().isActive = true;
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            this.GetComponent<BoxCollider2D>().enabled = false;
+        }
+
+        else if (isActive == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
             {
-                this.GetComponent<StagedDoor>().isActive = true;
-                parent.GetComponent<StagedDoor>().isActive = true;
+                if (parent.GetComponent<StagedDoor>())
+                    parent.GetComponent<StagedDoor>().isActive = true;
+                isActive = false;
+                timer = 0.2f;
                 this.GetComponent<SpriteRenderer>().enabled = false;
                 this.GetComponent<BoxCollider2D>().enabled = false;
             }
-
-            else if (isActive == true)
-            {
-                timer -= Time.deltaTime;
-                if (timer <= 0)
-                {
-                    if (parent.GetComponent<StagedDoor>())
-                        parent.GetComponent<StagedDoor>().isActive = true;
-                    isActive = false;
-                    timer = 0.2f;
-                    this.GetComponent<SpriteRenderer>().enabled = false;
-                    this.GetComponent<BoxCollider2D>().enabled = false;
-                }
-            }
         }
-        else if (collapsing == false && growing == true)
+    }
+
+    void growDoor()
+    {
+        GameObject parent = this.transform.parent.gameObject;
+        if (!parent.GetComponent<StagedDoor>() && this.GetComponent<SpriteRenderer>().enabled == false)
         {
-            if (!parent.GetComponent<StagedDoor>() && this.GetComponent<SpriteRenderer>().enabled == false)
+            this.GetComponent<StagedDoor>().isActive = true;
+            this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
+            this.GetComponent<SpriteRenderer>().enabled = true;
+            this.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else if (isActive == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
             {
-                this.GetComponent<StagedDoor>().isActive = true;
-                this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
+                if (this.transform.childCount != 0)
+                    this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
+                isActive = false;
+                timer = 0.2f;
                 this.GetComponent<SpriteRenderer>().enabled = true;
                 this.GetComponent<BoxCollider2D>().enabled = true;
             }
-            else if (isActive == true)
+        }
+    }
+
+    void resizeDoor(string growOrShrink)
+    {
+        GameObject parent = null;
+        if (this.transform.parent != null)
+            parent = this.transform.parent.gameObject;
+        if (parent != null && (transform.childCount == 0 && this.GetComponent<SpriteRenderer>().enabled == true) || (!parent.GetComponent<StagedDoor>() && this.GetComponent<SpriteRenderer>().enabled == false))
+        {
+            this.GetComponent<StagedDoor>().isActive = true;
+            if (growOrShrink == "shrink")
+                parent.GetComponent<StagedDoor>().isActive = true;
+            else if (growOrShrink == "grow")
+                this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
+            this.GetComponent<SpriteRenderer>().enabled = false;//= !this.GetComponent<SpriteRenderer>().enabled;
+            this.GetComponent<BoxCollider2D>().enabled = false;//= !this.GetComponent<BoxCollider2D>().enabled;
+        }
+        else if (isActive == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
             {
-                timer -= Time.deltaTime;
-                if (timer <= 0)
+                if (growOrShrink == "shrink" && parent.GetComponent<StagedDoor>())
                 {
-                    if (this.transform.childCount != 0)
-                        this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
-                    isActive = false;
-                    timer = 0.2f;
-                    this.GetComponent<SpriteRenderer>().enabled = true;
-                    this.GetComponent<BoxCollider2D>().enabled = true;
+                    parent.GetComponent<StagedDoor>().isActive = true;
+                    this.GetComponent<SpriteRenderer>().enabled = false;//!this.GetComponent<SpriteRenderer>().enabled;
+                    this.GetComponent<BoxCollider2D>().enabled = false;//!this.GetComponent<BoxCollider2D>().enabled;
                 }
+                else if (growOrShrink == "grow" && this.transform.childCount != 0)
+                {
+                    this.transform.GetChild(0).GetComponent<StagedDoor>().isActive = true;
+                    this.GetComponent<SpriteRenderer>().enabled = true;//!this.GetComponent<SpriteRenderer>().enabled;
+                    this.GetComponent<BoxCollider2D>().enabled = true;//!this.GetComponent<BoxCollider2D>().enabled;
+                }
+                isActive = false;
+                timer = 0.2f;
+                
             }
         }
-
-
     }
 
     void plateDepressed()
