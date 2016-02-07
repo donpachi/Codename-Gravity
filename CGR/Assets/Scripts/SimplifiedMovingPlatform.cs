4a80403cea6bd19d08713b9b756262bb2dfc82e5
Arthur Lee <s_s-money@hotmail.com>
2016-02-07 14:50:02 -0700

    Added cooldown for moving platform changing directions

--- CGR/Assets/Scripts/SimplifiedMovingPlatform.cs
+++ CGR/Assets/Scripts/SimplifiedMovingPlatform.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class SimplifiedMovingPlatform : MonoBehaviour {
 
     public int numberOfMovements = -1;
+    public float timer = 0;
 
 	// Use this for initialization
 	void Start () {
+        
 
         GameObject[] pushableObjects = GameObject.FindGameObjectsWithTag("Untagged");
         foreach (GameObject pushableObject in pushableObjects)
@@ -21,14 +23,17 @@ public class SimplifiedMovingPlatform : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (GetComponent<SliderJoint2D>().jointTranslation >= GetComponent<SliderJoint2D>().limits.max || GetComponent<SliderJoint2D>().jointTranslation <= GetComponent<SliderJoint2D>().limits.min && numberOfMovements != 0) 
+        if (timer > 0)
+            timer -= Time.deltaTime;
+        if (GetComponent<SliderJoint2D>().jointTranslation >= GetComponent<SliderJoint2D>().limits.max || GetComponent<SliderJoint2D>().jointTranslation <= GetComponent<SliderJoint2D>().limits.min && numberOfMovements != 0 && timer <= 0) 
         {
-            float newMotorSpeed = GetComponent<SliderJoint2D>().motor.motorSpeed * -1;
-            JointMotor2D newMotor = new JointMotor2D();
-            newMotor.motorSpeed = newMotorSpeed;
-            newMotor.maxMotorTorque = GetComponent<SliderJoint2D>().motor.maxMotorTorque;
-            GetComponent<SliderJoint2D>().motor = newMotor;
-            numberOfMovements--;
+                float newMotorSpeed = GetComponent<SliderJoint2D>().motor.motorSpeed * -1;
+                JointMotor2D newMotor = new JointMotor2D();
+                newMotor.motorSpeed = newMotorSpeed;
+                newMotor.maxMotorTorque = GetComponent<SliderJoint2D>().motor.maxMotorTorque;
+                GetComponent<SliderJoint2D>().motor = newMotor;
+                numberOfMovements--;
+                timer = 0.5f;
         }
 	}
 }
