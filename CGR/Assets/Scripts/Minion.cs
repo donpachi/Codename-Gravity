dccd775d060683dac7347816358644f5f6aa0845
Arthur Lee <s_s-money@hotmail.com>
2016-02-23 17:50:10 -0700

    Completed minion switching logic

--- CGR/Assets/Scripts/Minion.cs
+++ CGR/Assets/Scripts/Minion.cs
@@ -8,6 +8,7 @@ public class Minion : MonoBehaviour {
     GameObject minionAnchor;
     List<GameObject> minions = new List<GameObject>();
     GameObject parent;
+    float timer = 0.1f; 
 
 	// Use this for initialization
 	void Start () {
@@ -32,9 +33,9 @@ public class Minion : MonoBehaviour {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), minion.GetComponent<Collider2D>());
         }
 
+        GameObject prev = null;
         foreach (GameObject minion in minions)
-        {
-            GameObject prev = null;
+        {          
             if (minion == gameObject)
                 parent = prev;
             prev = minion;
@@ -44,16 +45,38 @@ public class Minion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (parent == null)
-            transform.position = Vector2.Lerp(transform.position, minionAnchor.transform.position, 0.1f);
-        else
-            transform.position = Vector2.Lerp(transform.position, transform.parent.position, 0.1f);
+            if (parent == null)
+                transform.position = Vector2.Lerp(transform.position, minionAnchor.transform.position, 0.1f);
+            else if (Vector2.Distance(transform.position, parent.transform.position) > 0.5f)
+                transform.position = Vector2.Lerp(transform.position, parent.transform.position, 0.1f);
 	}
 
     void swipeCheck(TouchController.SwipeDirection direction)
     {
-        if (direction == TouchController.SwipeDirection.UP && player.GetComponent<Player>().inMinionArea == true && gameObject.GetComponentsInChildren<Transform>().Length == 1)
+        minions = new List<GameObject>();
+        foreach (GameObject minion in GameObject.FindGameObjectsWithTag("Minion"))
         {
+            minions.Add(minion);
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), minion.GetComponent<Collider2D>());
+        }
+
+        foreach (GameObject minion in minions)
+        {
+            GameObject prev = null;
+            if (minion == gameObject)
+                parent = prev;
+            prev = minion;
+        }
+
+        if (direction == TouchController.SwipeDirection.UP && player.GetComponent<Player>().inMinionArea == true && minions[0] == gameObject)
+        {
+            foreach (GameObject minion in minions)
+            {
+                GameObject prev = null;
+                if (minion == gameObject)
+                    parent = prev;
+                prev = minion;
+            }
             switchControl();
         }
     }
