c253dd78962ad748f7d3fb5547f88f930b8ee8ce
Yosuke Saito <yosuke.saito@outlook.com>
2016-02-23 22:38:05 -0700

    Updated rail. it now follows rail properly. starting to add control

--- CGR/Assets/Scripts/FollowPlayer.cs
+++ CGR/Assets/Scripts/FollowPlayer.cs
@@ -8,14 +8,26 @@ public class FollowPlayer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
-        position = new Vector3(0, 0, -9);
-	}
+        position = new Vector3(player.transform.position.x, player.transform.position.y, -9);
+        transform.position = position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        position.x = player.transform.position.x;
-        position.y = player.transform.position.y;
+        Vector2 newPostion = Vector2.Lerp(transform.position, player.transform.position, 0.5f);
 
-        gameObject.transform.position = position;
+        position.x = newPostion.x;
+        position.y = newPostion.y;
+
+        transform.position = position;
 	}
+    
+    /// <summary>
+    /// Sets the focal point of the camera
+    /// </summary>
+    /// <param name="focusPoint"></param>
+    public void setFollowObject(GameObject focusPoint)
+    {
+        player = focusPoint;
+    }
 }
---
+++ CGR/Assets/Scripts/RailBoxControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailBoxControl : MonoBehaviour {
+
+    //defined in unity
+    public float THRUST;
+    public float MAXSPEED;
+
+    Rigidbody2D objectRb;
+    bool entered;
+    bool active;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+        entered = false;
+        objectRb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && entered && objectRb.velocity.magnitude < MAXSPEED)
+            applyMoveForce(THRUST);
+    }
+
+    void applyMoveForce(float force)
+    {
+        TouchController.TouchLocation movementDirection = TouchController.Instance.getTouchDirection();
+
+        switch (movementDirection)
+        {
+            case TouchController.TouchLocation.LEFT:
+                objectRb.AddForce(OrientationListener.instanceOf.getWorldLeftVector() * force, ForceMode2D.Impulse);
+                break;
+            case TouchController.TouchLocation.RIGHT:
+                objectRb.AddForce(OrientationListener.instanceOf.getWorldRightVector() * force, ForceMode2D.Impulse);
+                break;
+            case TouchController.TouchLocation.NONE:
+                break;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionEvent)
+    {
+        if(collisionEvent.gameObject.name == "Player")
+        {
+            entered = true;
+        }
+    }
+
+
+
+}
