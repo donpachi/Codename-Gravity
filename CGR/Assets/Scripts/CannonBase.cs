8e0e8a3be670625be48a83ec216eac5b0e05c7d9
Yosuke Saito <yosuke.saito@outlook.com>
2016-04-04 13:39:40 -0600

    Cannon Animation

---
+++ CGR/Assets/Scripts/CannonBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBase : MonoBehaviour {
+    private GravityCannon cannon;
+
+	// Use this for initialization
+	void Start () {
+        cannon = GetComponentInChildren<GravityCannon>();
+	}
+
+    void EnableFireButton()
+    {
+        cannon.EnableFireButton();
+    }
+}
