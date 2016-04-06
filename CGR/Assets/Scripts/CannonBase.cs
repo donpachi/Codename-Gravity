d16610d80546818b91c27b0cdbb622ccff7eb077
Yosuke Saito <yosuke.saito@outlook.com>
2016-04-06 16:30:43 -0600

    updated cannon animation. now has different angle cycles to choose from

--- CGR/Assets/Scripts/CannonBase.cs
+++ CGR/Assets/Scripts/CannonBase.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Fire Modes:
+/// 0 - 180 degrees
+/// 1 - 90 deg right
+/// 2 - 90 deg middle
+/// 3 - 90 deg left
+/// </summary>
 public class CannonBase : MonoBehaviour {
+    public int CannonMode = 0;
+
     private GravityCannon cannon;
 
 	// Use this for initialization
 	void Start () {
         cannon = GetComponentInChildren<GravityCannon>();
+        GetComponent<Animator>().SetInteger("CannonMode", CannonMode);
 	}
 
     void EnableFireButton()
     {
         cannon.EnableFireButton();
+    }
+
+    public void SetFireMode(int mode)
+    {
+        CannonMode = mode;
+        GetComponent<Animator>().SetInteger("CannonMode", CannonMode);
     }
 }
