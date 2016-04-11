177328db91788089fcd5aca7426fa2fd733e5d07
Yosuke Saito <yosuke.saito@outlook.com>
2016-04-11 15:58:55 -0600

    vortex prefab update

--- CGR/Assets/Scripts/GravityVortex.cs
+++ CGR/Assets/Scripts/GravityVortex.cs
@@ -24,12 +24,6 @@ public class GravityVortex : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//float distance = Vector2.Distance (player.GetComponent<Transform>().position, this.GetComponent<Transform> ().position);
-
-		//if (distance < VORTEXDISTANCE && !player.GetComponent<Player>().IsInTransition()) {
-		//	Vector2 direction = (this.GetComponent<Transform>().position - player.GetComponent<Transform> ().position).normalized;
-		//	player.GetComponent<Rigidbody2D>().AddForce(direction * VORTEXFORCE * (distance / VORTEXDISTANCE));
-		//}
 	}
 
     void OnTriggerStay2D(Collider2D collision)
@@ -42,7 +36,6 @@ public class GravityVortex : MonoBehaviour {
             {
                 Vector2 direction = (this.GetComponent<Transform>().position - obj.GetComponent<Transform>().position).normalized;
                 obj.GetComponent<Rigidbody2D>().AddForce(direction * currentForce * ((VORTEXDISTANCE - distance) / VORTEXDISTANCE));
-                Debug.Log("Distance: " + distance + " Force Applied: " + currentForce * ((VORTEXDISTANCE - distance) / VORTEXDISTANCE));
             }
         }
     }
