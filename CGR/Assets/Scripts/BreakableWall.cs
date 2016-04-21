d49379d66ac9f9f5a61f624233588a726e6788f5
Rajan Saini <rajansaini73@hotmail.com>
2016-04-20 18:11:30 -0600

    Pushing changes to level 8

--- CGR/Assets/Scripts/BreakableWall.cs
+++ CGR/Assets/Scripts/BreakableWall.cs
@@ -17,9 +17,7 @@ public class BreakableWall : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collisionInfo) {
-        Debug.Log(collisionInfo.relativeVelocity.magnitude + "  " + breakThreshold);
         if ((collisionInfo.gameObject.tag == "Pushable" || collisionInfo.gameObject.tag == "Boulder" || collisionInfo.gameObject.tag == "Hazard") && collisionInfo.relativeVelocity.magnitude > breakThreshold) {
-            Debug.Log(collisionInfo.relativeVelocity.magnitude + "  " + breakThreshold);
             this.GetComponent<Collider2D>().enabled = false;
             anima.SetTrigger("destroy");
 		}
