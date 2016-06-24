721ad7d72dbbe13d467934d687da8c8d2a3abf4d
Yosuke Saito <yosuke.saito@outlook.com>
2016-06-24 16:33:18 -0600

    Added reset to boulder. Added dead zone to pinch zoom

--- CGR/Assets/Scripts/ResetPosition.cs
+++ CGR/Assets/Scripts/ResetPosition.cs
@@ -3,15 +3,53 @@ using System.Collections;
 
 public class ResetPosition : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector2 savePosition;
+    private Rigidbody2D rBody;
 
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void CheckpointRestart()
+    {
+        if (savePosition == null)
+        {
+            Debug.LogError("Object does not have a save state");
+            return;
+        }
+        gameObject.transform.position = savePosition;
+        if (rBody)
+            StartCoroutine(freezeObject(2));
+            
+    }
+
+    void SetCheckpointState()
+    {
+        savePosition = gameObject.transform.position;
+    }
+
+    IEnumerator freezeObject(float time)
+    {
+        float formerGravity = rBody.gravityScale;
+        rBody.gravityScale = 0;
+        rBody.Sleep();
+        yield return new WaitForSeconds(time);
+
+        rBody.gravityScale = formerGravity;
+    }
+
+    //Listeners for player
+    void OnEnable()
+    {
+        LevelManager.OnCheckpointLoad += CheckpointRestart;
+        LevelManager.OnCheckpointSave += SetCheckpointState;
+    }
+
+    void OnDisable()
+    {
+        LevelManager.OnCheckpointLoad -= CheckpointRestart;
+        LevelManager.OnCheckpointSave -= SetCheckpointState;
+    }
 
 }
